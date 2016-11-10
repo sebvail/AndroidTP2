da7f12d4cdf4e6ba9d902ecf03446a4a839e95c5
gabrieldube <g_dube96@hotmail.fr>
2016-11-09 20:29:41 -0500

    tamere

---
+++ app/src/main/java/com/example/snowt/tp2/QREncoder.java
@@ -0,0 +1,40 @@
+package com.example.snowt.tp2;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+
+/**
+ * Created by Gabriel on 09/11/2016.
+ */
+public class QREncoder {
+
+
+    public Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 150, 150, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 150, 0, 0, w, h);
+        return bitmap;
+    }
+}
