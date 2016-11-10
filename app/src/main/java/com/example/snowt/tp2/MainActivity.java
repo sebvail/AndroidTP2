0d3b8771d72a7fd0984059ea2ba1521a39ffa4f7
gabrieldube <g_dube96@hotmail.fr>
2016-11-10 03:55:38 -0500

    Fuck.This.Shit.

--- app/src/main/java/com/example/snowt/tp2/MainActivity.java
+++ app/src/main/java/com/example/snowt/tp2/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,6 +35,9 @@ import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.snowt.tp2.R.id.fragment_scan;
+import static com.example.snowt.tp2.R.id.scan_from_fragment;
+
 public class MainActivity extends AppCompatActivity {
 
     /**
@@ -44,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
+    Bitmap bmp;
+    ImageView iv2;
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
@@ -53,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     ArrayList<Information> listeEnvoi;
+    public void ajouterListe(String info){
+        listeEnvoi.add(new Information(info));
+        updateAdapterListeEnvoi();
+    }
     public void retirerListe(String info){
         listeEnvoi.remove(new Information(info));
         updateAdapterListeEnvoi();
@@ -80,6 +90,13 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        listeEnvoi.add(new Information("info1"));
+        listeEnvoi.add(new Information("info2"));
+        listeEnvoi.add(new Information("info3"));
+        listeEnvoi.add(new Information("info4"));
+        listeEnvoi.add(new Information("info5"));
+
     }
 
 
@@ -103,5 +120,207 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+
+        View  rootView;
+
+        View views;
+        @Override
+        public View onCreateView( LayoutInflater inflater,  ViewGroup container,Bundle savedInstanceState)
+        {
+            Bitmap bmp;
+            ImageView iv2;
+            final ArrayList<Information> listeEnvoi = new ArrayList<>();
+            listeEnvoi.add(new Information("info1"));
+            listeEnvoi.add(new Information("info2"));
+            listeEnvoi.add(new Information("info3"));
+            listeEnvoi.add(new Information("info4"));
+            listeEnvoi.add(new Information("info5"));
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)){
+                case 1: {
+                    Fragment fragment = getFragmentManager().findFragmentByTag("android:switcher:2131558525:0");
+                    if(rootView != null) {
+                        List<Fragment> frg = new ArrayList<Fragment>();
+                        frg = getFragmentManager().getFragments();
+                        getFragmentManager().beginTransaction().remove(fragment).commit();
+                        rootView = inflater.inflate(R.layout.activity_scan, container, false);
+                    }
+                    else {
+                        rootView = inflater.inflate(R.layout.activity_scan, container, false);
+                        iv2 = (ImageView) rootView.findViewById(R.id.imageView);
+                        bmp = QREncoder.encodeAsBitmap("yo");
+                        iv2.setImageBitmap(bmp);
+                    }
+                    break;
+                }
+                case 2: {
+                    rootView = inflater.inflate(R.layout.envoi_layout,container, false);
+                    views = inflater.inflate(R.layout.activity_scan, container, false);
+                    (rootView.findViewById(R.id.btnEnvoyerInfo)).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txt = (EditText)rootView.findViewById(R.id.editAjouterEnvoi);
+                            String text = txt.getText().toString();
+                            Bitmap bmp = QREncoder.encodeAsBitmap(text);
+                            ImageView iv2 = (ImageView)views.findViewById(R.id.imageView);
+                            iv2.setImageBitmap(bmp);
+                            //0x7f0d007d
+                            Fragment lo = getFragmentManager().findFragmentByTag("android:switcher:2131558525:0");
+
+                            final android.support.v4.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                            ft.detach(lo);
+                            ft.attach(lo);
+                            ft.commit();
+
+                        }
+                    });
+                    (rootView.findViewById(R.id.btnAjouterEnvoi)).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txt = (EditText)rootView.findViewById(R.id.editAjouterEnvoi);
+                            String text = txt.getText().toString();
+                            listeEnvoi.add(new Information(text));
+                            ListView lv = (ListView)rootView.findViewById(R.id.lv_envoi);
+                            CustomAdapterInfoEnvoi adapter = new CustomAdapterInfoEnvoi(getActivity().getBaseContext(),R.layout.envoi_layout,listeEnvoi);
+                            lv.setAdapter(adapter);
+
+                        }
+                    });
+                    (rootView.findViewById(R.id.btnSupprimerInfo)).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ((MainActivity)getActivity()).retirerListe(rootView.findViewById(R.id.txtEnvoiInfo).toString());
+
+                        }
+                    });
+                    break;
+                }
+                case 3: {
+                    rootView = inflater.inflate(R.layout.recu_layout,container,false);
+                    break;
+                }
+            }
+
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "scan";
+                case 1:
+                    return "À envoyer";
+                case 2:
+                    return "Recu";
+            }
+            return null;
+        }
+    }
+
+    public static class ScanFragment extends Fragment {
+        private String toast;
+
+        public ScanFragment() {
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+            displayToast();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_scan, container, false);
+            Button scan = (Button) view.findViewById(R.id.scan_from_fragment);
+            scan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    scanFromFragment();
+                }
+            });
+            return view;
+        }
+
+        public void scanFromFragment() {
+            IntentIntegrator.forSupportFragment(this).initiateScan();
+        }
+
+        private void displayToast() {
+            if(getActivity() != null && toast != null) {
+                Toast.makeText(getActivity(), toast, Toast.LENGTH_LONG).show();
+                toast = null;
+            }
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if(result != null) {
+                if(result.getContents() == null) {
+                    toast = "Cancelled from fragment";
+                } else {
+                    toast = "Scanned from fragment: " + result.getContents();
+                }
+
+                // At this point we may or may not have a reference to the activity
+                displayToast();
+            }
+        }
     }
 }
